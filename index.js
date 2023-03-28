3985493f1154b2c858aabbd91d0f31b3f00036d4
jlgerbino <joselgerbino@gmail.com>
2023-03-28 14:30:32 -0300

    arreglos desafio

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const manager = new ProductManager();
 
 const env = async() =>{    
     
-    //let carga = await manager.addProduct("Cama", "Colchon 140 x 190", 82900,"no imagen", 20, 20);
+    //let carga = await manager.addProduct("Colchon", "Colchon 140 x 190", 82900,"no imagen", 1, 20);
     
     //let productos = await manager.getProducts(); console.log(productos)
     
-    //let id = await manager.getProductsById(3);
+    //let id = await manager.getProductsById(3); console.log(id)
     
     //let borrar = await manager.deleteProduct(4); console.log(borrar)
 
-    //let modificar = await manager.updateProduct(3, "Mesa tv","casa puerta", 10, "si imagen",3,10); console.log(modificar)
+    let modificar = await manager.updateProduct(5, "Casa","casa grande", 10, "sin imagen",4,10); console.log(modificar)
 }
 
 env()
