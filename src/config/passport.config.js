8423bdff1150c9c26073037082e573e02fee60c1
jlgerbino <joselgerbino@gmail.com>
2023-06-08 07:39:31 -0300

    segunda p integradora

--- src/config/passport.config.js
+++ src/config/passport.config.js
@@ -81,6 +81,7 @@ const initializePassport = () => {
                         email: email,
                         age: 18,
                         password:"",
+                        cartId: cartId,//                        
                 }
                 const result = await userModel.create(newUser);
                 done(null,result)
